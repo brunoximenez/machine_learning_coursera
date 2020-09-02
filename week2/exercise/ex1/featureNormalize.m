a39e9b1ae09f288fa1c61b2757778c5c77408402
bruno <bruno.ximenez@obspm.fr>
2020-09-02 16:29:38 +0200

    Added one line of code on computeCost.m (equivalent) and have now latex report.
    The line of code was just an equivalent way of computing the cost function.
    The report folder contains the pdf report plus the latex code.
    The summary of week 2 is written.

--- week2/exercise/ex1/featureNormalize.m
+++ week2/exercise/ex1/featureNormalize.m
@@ -26,7 +26,7 @@ sigma = zeros(1, size(X, 2));
 % Hint: You might find the 'mean' and 'std' functions useful.
 %       
 
-    mu = mean(X_norm)
+    mu = mean(X_norm);
     sigma = std(X_norm);
     X_norm = (X_norm - mu) ./ sigma;
 
